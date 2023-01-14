e559194bd17d372e909072e5a865c06f328c3694
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2023-01-14 17:07:34 +0100

    Added pagination to search page

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from movies import models as movies_models
 from django.views import View
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.db.models import Q
 
 
@@ -37,14 +38,30 @@ class Search(View):
         url = request.META.get("HTTP_REFERER")
         query = request.GET.get("query", None)
         try:
+            url = request.get_full_path()
             if query is not None:
-                movies = movies_models.Movie.objects.filter(
-                    Q(title__icontains=query) | Q(description__icontains=query) | Q(category__name__icontains=query)
-                ).distinct()
+                movies = (
+                    movies_models.Movie.objects.filter(
+                        Q(title__icontains=query) | Q(description__icontains=query) | Q(category__name__icontains=query)
+                    )
+                    .distinct()
+                    .order_by("-created")
+                )
                 if movies.count() <= 0:
                     messages.error(request, "No result found")
                     raise SearchException()
-                context = {"movies": movies, "query": query}
+                paginator = Paginator(movies, 1)
+                if "page" in url:
+                    page_str = str(url).split("page")[1]
+                    page = page_str.split("=")[1]
+                    print(page)
+                else:
+                    page = 1
+                movies = paginator.get_page(page)
+                full_url = request.get_full_path()
+                if "page" in str(full_url):
+                    full_url = str(full_url).split("&")[0]
+                context = {"movies": movies, "query": query, "full_url": full_url}
                 return render(request, "core/search.html", context)
             messages.error(request, "You didn't fill the search form")
             raise SearchException()
